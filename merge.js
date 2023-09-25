af33aa6658272ee89a369693e4eba3967a026dc8
unknown <yagoubi.aek.2@gmail.com>
2023-09-25 15:49:37 +0100

    delete unlink

--- merge.js
+++ merge.js
@@ -12,6 +12,13 @@ const port = 3000
 
 app.post('/merge', (req, res) => {
 
+  
+        const base64Data = req.body.imgBase64.replace(/^data:image\/png;base64,/, "");
+        fs.writeFile("image.png", base64Data, 'base64', function (err) {
+            if (err) {
+                console.log(err);
+            }
+    
             const cmd = `ffmpeg -i video2.mp4 -i image.png \
     -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,${req.body.from},${req.body.to})'" \
     -pix_fmt yuv420p -c:a copy \
