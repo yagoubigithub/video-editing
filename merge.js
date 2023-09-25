7c3bfadf4c338b04be2009d17e4023d2b6c902f2
unknown <yagoubi.aek.2@gmail.com>
2023-09-25 07:42:47 +0100

    first commit

---
+++ merge.js
@@ -0,0 +1,53 @@
+console.log(3333)
+const exec = require('child_process').exec;
+
+
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const app = express()
+app.use(cors())
+app.use(express.json());
+const port = 3000
+
+app.post('/merge', (req, res) => {
+
+    fs.unlink('./output.mp4', (err) => {
+        if (err) console.log(err);
+        const base64Data = req.body.imgBase64.replace(/^data:image\/png;base64,/, "");
+        fs.writeFile("image.png", base64Data, 'base64', function (err) {
+            if (err) {
+                console.log(err);
+            }
+    
+            const cmd = `ffmpeg -i video2.mp4 -i image.png \
+    -filter_complex "[0:v][1:v] overlay=0:0:enable='between(t,${req.body.from},${req.body.to})'" \
+    -pix_fmt yuv420p -c:a copy \
+    output.mp4`;
+            exec(cmd, function (err, stdout, stderr) {
+    
+    
+                if (err) {
+                    console.log(err)
+                    //throw err;
+                }
+    res.json({success : true})
+    
+    
+    
+            })
+        });
+      });
+   
+
+
+
+})
+
+app.get('/download', function(req, res){
+    const file = `${__dirname}/output.mp4`;
+    res.download(file); // Set disposition and send it.
+  });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ script.js
@@ -0,0 +1,354 @@
+let c = 0
+let insertText = false
+const canvasTxt = window.canvasTxt
+let w, h, scaleH, scaleW;
+let one_second = 0;
+let duration
+let from = 0,
+  to = 3
+const videoContainer = document.getElementById('video-container')
+const download_btn = document.getElementById('download_btn')
+const theCanvas = document.createElement("canvas")
+const theContext = theCanvas.getContext("2d")
+
+async function showFram() {
+  let videoUrl = "./video2.mp4"
+  let videoBlob = await fetch(videoUrl).then((r) => r.blob());
+  let videoObjectUrl = URL.createObjectURL(videoBlob);
+  let video = document.createElement("video");
+  video.src = videoObjectUrl;
+
+  let canvas = document.createElement("canvas");
+  let context = canvas.getContext("2d");
+
+  if (download_btn) {
+    download_btn.addEventListener("click", () => {
+      const dataurl = theCanvas.toDataURL()
+      download_btn.innerText = "downloading ..."
+
+      fetch("http://localhost:3000/merge",
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+
+
+          body: JSON.stringify({
+            imgBase64: dataurl,
+            from,
+            to
+          })
+        }).then((response) => response.json()).then((result) => {
+          download_btn.innerText = "download"
+          if (result.success) {
+            const a = document.createElement('a');
+            document.body.appendChild(a);
+            a.download = 'output.mp4';
+            a.href = "http://localhost:3000/download";
+            a.click();
+          }
+        })
+
+
+    })
+  }
+
+  canvas.addEventListener('click', (event) => clickCanvas(canvas, context, event.offsetX, event.offsetY))
+
+
+  const myRange = document.getElementById("myRange")
+  let frominput = false;
+
+
+
+  myRange.value = 0
+  let fired = 0
+  video.addEventListener("canplay", async function () {
+    // if (seekResolve) seekResolve();
+
+    
+    fired++
+    if (fired < 4) {
+      console.log("fired")
+      scaleW = (document.body.getBoundingClientRect().width - 400) / video.videoWidth;
+      scaleH = (document.body.getBoundingClientRect().height - 200) / video.videoHeight;
+      [w, h] = [document.body.getBoundingClientRect().width - 400, document.body.getBoundingClientRect().height - 200];
+
+
+      canvas.width = w;
+      canvas.height = h;
+
+      theCanvas.width = video.videoWidth;
+      theCanvas.height = video.videoHeight;
+
+      myRange.max = video.duration
+      myRange.style.width = w + "px"
+
+      video.currentTime = 1 
+      video.currentTime = 0  
+     
+      one_second = w / video.duration;
+      timing.style.width = (one_second * 3) + "px";
+      time.innerHTML = new Date(from * 1000).toISOString().substring(14, 19) + "---" + new Date(to * 1000).toISOString().substring(14, 19)
+
+      duration = video.duration
+      context.drawImage(video, 0, 0, w, h);
+      document.getElementById("video-container").append(canvas)
+      videoContainer.style.width = w + "px";
+      videoContainer.style.height = h + "px";
+      print()
+    }
+
+
+
+  });
+
+  myRange.addEventListener("input", (event) => {
+    console.log(event.target.value)
+    video.currentTime = 1
+   
+    video.currentTime = parseInt(event.target.value)
+    context.drawImage(video, 0, 0, w, h);
+    frominput = true
+  })
+
+
+
+ const play_btn =  document.getElementById("play-btn")
+
+ let toggele = 'pause'
+ play_btn.addEventListener("click" , (ev)=>{
+  if(toggele === 'pause'){
+    video.play()
+    toggele = 'play'
+  }else{
+    video.pause()
+    toggele = 'pause'
+  }
+  video.addEventListener('play', function() {
+    var $this = this; //cache
+    (function loop() {
+      if (!$this.paused && !$this.ended) {
+       
+        context.drawImage(video, 0, 0, w, h);
+        setTimeout(loop, 1000 / 30); // drawing at 30fps
+      }
+    })();
+  }, 0);
+
+  video.addEventListener('timeupdate', function() {
+   console.log( video.currentTime   )
+   myRange.value = video.currentTime 
+  });
+ })
+
+  //context.drawImage(video, 0, 0, w, h);
+
+
+
+
+
+
+}
+
+
+function prepareInsertTetx() {
+  document.body.style.cursor = "text";
+  insertText = true
+}
+
+
+function clickCanvas(canvas, context, x, y) {
+  if (insertText) {
+    var input = document.createElement("span");
+
+    input.setAttribute("contenteditable", "true")
+
+    input.className = "my-input"
+    input.style.position = "absolute";
+    input.style.zIndex = 100;
+    input.style.border = 0;
+    input.style.color = "red";
+    input.style.background = "transparent";
+    input.style.fontFamily = "Calibri";
+    input.style.fontSize = (25) + "px";
+    input.style.padding = 0;
+    input.style.margin = 0;
+    input.style.position = "absolute"
+
+
+
+
+    input.style.left = x + "px";
+    input.style.top = (y) + "px";
+
+
+    videoContainer.appendChild(input);
+
+    window.setTimeout(function () {
+      input.focus()
+    }, 33);
+
+    input.addEventListener("blur", () => {
+      const txtCanvas = document.createElement('canvas')
+      txtCanvas.style.pointerEvents = 'none';
+      const textContext = txtCanvas.getContext("2d");
+      txtCanvas.width = w;
+      txtCanvas.height = h;
+      insertText = false
+      document.body.style.cursor = "default";
+      const textToWrite = input.innerText;
+
+      textContext.fillStyle = '#ff0000' //red color text
+      canvasTxt.drawText(textContext, textToWrite, { x, y, width: input.offsetWidth + 10, height: input.offsetHeight + 10, fontSize: 25, font: "Calibri", align: "left" })
+
+      theContext.fillStyle = '#ff0000' //red color text
+      canvasTxt.drawText(theContext, textToWrite, { x: x / scaleW, y: y / scaleH, width: (input.offsetWidth + 10) / scaleW, height: (input.offsetHeight + 10) / scaleH, fontSize: 25 / scaleW, font: "Calibri", align: "left" })
+
+
+
+      videoContainer.removeChild(input)
+      videoContainer.append(txtCanvas)
+
+
+
+
+
+    })
+
+
+
+  }
+
+}
+
+
+showFram()
+
+/***************************************************************************************** */
+const timing = document.getElementById("timing");
+
+const time = document.getElementById("time");
+const timing_left = document.getElementById("timing-left");
+const timing_right = document.getElementById("timing-right")
+let original_width = 0;
+let original_x = 0;
+let original_y = 0;
+let original_mouse_x = 0;
+let original_mouse_y = 0;
+let minwidth = 100;
+
+timing_right.addEventListener('mousedown', function (e) {
+  e.preventDefault()
+  window.addEventListener('mousemove', resize)
+  window.addEventListener('mouseup', () => {
+    window.removeEventListener('mousemove', resize)
+  })
+
+  function resize(e) {
+
+
+    if (w - e.pageX + 200 <= 0) {
+      return
+    }
+
+    if (e.pageX - timing.getBoundingClientRect().left <= minwidth) return
+
+    timing.style.width = e.pageX - timing.getBoundingClientRect().left + 'px';
+    to = (e.pageX - 200) / one_second
+    time.innerHTML = new Date(from * 1000).toISOString().substring(14, 19) + "---" + new Date(to * 1000).toISOString().substring(14, 19)
+
+
+
+  }
+
+})
+
+
+timing_left.addEventListener('mousedown', function (e) {
+  e.preventDefault()
+  original_width = parseFloat(getComputedStyle(timing, null).getPropertyValue('width').replace('px', ''));
+  original_x = timing.getBoundingClientRect().left;
+
+  original_mouse_x = e.pageX;
+  original_mouse_y = e.pageY;
+  window.addEventListener('mousemove', resize)
+  window.addEventListener('mouseup', () => {
+    window.removeEventListener('mousemove', resize)
+  })
+
+  function resize(e) {
+
+    if (e.pageX - original_mouse_x < 0) {
+      return
+    }
+    if (original_width - (e.pageX - original_mouse_x) <= minwidth) return
+
+    timing.style.width = original_width - (e.pageX - original_mouse_x) + 'px'
+    timing.style.left = (e.pageX - original_mouse_x) + 'px'
+
+    from = (e.pageX - original_mouse_x) / one_second
+
+    time.innerHTML = new Date(from * 1000).toISOString().substring(14, 19) + "---" + new Date(to * 1000).toISOString().substring(14, 19)
+
+
+
+  }
+
+})
+/*********************************************************************************************** */
+
+const add_btn = document.getElementById("add-btn");
+const remove_btn = document.getElementById("remove-btn");
+const font_size_input = document.getElementById("font-size-input")
+let font_size = 55;
+const text_div = document.getElementById("text")
+
+font_size_input.addEventListener("input", (ev) => {
+  const value = parseInt(ev.target.value)
+  font_size = value
+  font_size_input.value = value
+  text_div.style.fontSize = value + "px"
+})
+
+add_btn.addEventListener("click", (ev) => {
+  font_size++;
+  font_size_input.value = font_size
+  text_div.style.fontSize = font_size + "px"
+
+})
+
+remove_btn.addEventListener("click", (ev) => {
+  if (font_size === 11) return
+  font_size--;
+  font_size_input.value = font_size
+  text_div.style.fontSize = font_size + "px"
+
+})
+
+text_div.addEventListener("input", (ev) => {
+  print()
+})
+
+
+function print() {
+
+  theContext.fillStyle = '#ffffff' //white color text
+  theContext.clearRect(0, 0, theCanvas.width, theCanvas.height)
+  canvasTxt.drawText(theContext, text_div.innerText, { x: text_div.getBoundingClientRect().left, y: text_div.getBoundingClientRect().top, width: (text_div.offsetWidth + 10) / scaleW, height: (text_div.offsetHeight + 10) / scaleH, fontSize: font_size / scaleW, font: "Calibri", align: "center" })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
