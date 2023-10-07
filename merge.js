3aa83afefd31bd6ef9653f5c84ae5dfc371be7fc
unknown <yagoubi.aek.2@gmail.com>
2023-10-07 23:40:27 +0100

    add soket.io

--- merge.js
+++ merge.js
@@ -18,7 +18,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = socketIo(server,{ 
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+}) //in case server and client run on different urls
 
 
 
